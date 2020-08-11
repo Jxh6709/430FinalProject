9d7ff19eac916b9c1a3f919a109efa6614f0f21e
Joshua Haber <jxh6709@rit.edu>
2020-08-11 12:46:41 -0400

    Removed hardcoded email

--- server/models/emailHandler.js
+++ server/models/emailHandler.js
@@ -33,7 +33,7 @@ const sendEmail = (body, docBuf, nameOfFile) => {
   // all of the options conveniently in an object
   const mailOptions = {
     from: 'jxh6709@g.rit.edu',
-    to: 'mabcst@rit.edu',
+    to: body.email,
     subject: body.subject,
     text: body.content,
     bcc: 'mabcst@rit.edu',
